6688e6dfaa2a51fb54643e93156962f509c0a6df
SerjMartin <serjmartin@mail.com>
2022-01-25 20:28:24 +0000

    Added sorting JS and back to top link

--- products/views.py
+++ products/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
+from django.db.models.functions import Lower
+
 from .models import Product, Category
 
 # Create your views here.
 
 def all_products(request):
-    """ A view to show al products, including sorting and search queries """
+    """ A view to show all products, including sorting and search queries """
 
     products = Product.objects.all()
-
-    """ return that template properly when we are not using any sorting """
     query = None
     categories = None
     sort = None
@@ -58,7 +58,7 @@ def all_products(request):
 
 
 def product_detail(request, product_id):
-    """ A view to show individual product detail """
+    """ A view to show individual product details """
 
     product = get_object_or_404(Product, pk=product_id)
 
