fec01b5b3c870beaca94a5e7f48fc84d688a10ab
SerjMartin <serjmartin@mail.com>
2022-02-16 19:28:44 +0000

    add env file

---
+++ env_.py
@@ -0,0 +1,3 @@
+import os
+
+os.environ["DATABASE_URL"] = "postgres://xdmhxjhvpghcrw:8bd39ac52bd0c689b35efded3b09847ad140b7a616ec9ab2feaa93fe25e11968@ec2-34-250-19-18.eu-west-1.compute.amazonaws.com:5432/d2pegonuk5u0va"
